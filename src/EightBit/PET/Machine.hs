c35eb3ca81898cb7fae2b15c2a338a876104cecf
Dr. ERDI Gergo <gergo@erdi.hu>
2015-02-25 19:46:12 +0800

    Changable foreground color

--- src/EightBit/PET/Machine.hs
+++ src/EightBit/PET/Machine.hs
@@ -7,6 +7,7 @@ import EightBit.PET.Video
 import EightBit.PET.Keyboard
 
 import Language.KansasLava
+import Language.KansasLava.Signal.Utils (debounce)
 import Hardware.KansasLava.Boards.Papilio.Arcade
 import Hardware.KansasLava.VGA.Driver
 import Hardware.KansasLava.VGA
@@ -14,6 +15,9 @@ import Hardware.KansasLava.PS2
 
 import Data.ByteString (ByteString)
 import Data.Sized.Unsigned
+import Data.Sized.Ix
+import Data.Sized.Matrix ((!))
+import qualified Data.Sized.Matrix as Matrix
 import Data.Bits
 
 machine :: ByteString -> ByteString -> Fabric ()
@@ -21,8 +25,26 @@ machine fontImage kernalImage = do
     (ps2A, _) <- ps2
     let keyboardEvent = eventPS2 . decodePS2 . samplePS2 $ ps2A
 
+    (buttonUp, buttonDown) <- do
+        Buttons{..} <- buttons
+        let (_, buttonUp', _) = debounce (Witness :: Witness X16) buttonUp
+            (_, buttonDown', _) = debounce (Witness :: Witness X16) buttonDown
+        return (buttonUp', buttonDown')
+
+    let palette = Matrix.fromList [ (0x0, 0xF, 0x0)
+                                  , (0xF, 0xF, 0xF)
+                                  , (0xF, 0xB, 0x0)
+                                  , (0x0, 0x8, 0xF)
+                                  ]
+        color = funMap (Just . (palette !)) $ runRTL $ do
+            r <- newReg (0 :: U2)
+            CASE [ IF buttonUp $ r := reg r + 1
+                 , IF buttonDown $ r := reg r - 1
+                 ]
+            return $ reg r
+
     let (textRAM, keyboardRowSel, _) = board kernalImage (vgaOutVBlank video) keyboardRow
-        (TextOut{..}, video) = text40x25 (pureS maxBound) TextIn{..}
+        (TextOut{..}, video) = text40x25 color TextIn{..}
         KeyboardOut{..} = keyboard KeyboardIn{..}
 
         fontAddr :: Signal CLK U11
